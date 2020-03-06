cd4809c0b690dd1b0f7eceba001406db6d7d414e
Simas <simas@simas-dei.com>
2020-03-06 19:55:17 +0200

    test add posts action

--- src/state/reducers/posts/reducer.test.ts
+++ src/state/reducers/posts/reducer.test.ts
@@ -6,4 +6,27 @@ describe('Posts Reducer', () => {
     const newState = postsReducer(undefined, {});
     expect(newState).toEqual([]);
   });
+
+  it('should return New State if recieving action type', () => {
+    const posts = [
+      {
+        id: 1,
+        title: 'test 1',
+      },
+      {
+        id: 2,
+        title: 'test 2',
+      },
+      {
+        id: 3,
+        title: 'test 3',
+      },
+    ];
+    const newState = postsReducer(undefined, {
+      type: types.GET_POSTS,
+      payload: posts,
+    });
+
+    expect(newState).toEqual(posts);
+  });
 });
